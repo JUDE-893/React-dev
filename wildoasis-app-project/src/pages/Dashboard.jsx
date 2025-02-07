53f5c2e2af7e50e5e40bea1a0259dc98df9f76a3
JUDE-893 <ichbinarif01@gmail.com>
2025-02-07 12:01:38 +0100

    update: --other

--- wildoasis-app-project/src/pages/Dashboard.jsx
+++ wildoasis-app-project/src/pages/Dashboard.jsx
@@ -1,13 +1,14 @@
 import StyledDashboardLayout from '../features/dashboard/DashboardLayout'
 import Heading from "../ui/Heading";
 import Row from "../ui/Row";
+import HeadingMenu from "../ui/HeadingMenu";
 
 function Dashboard() {
   return (
     <>
     <Row type="horizontal">
       <Heading as="h1">Dashboard</Heading>
-      <p style={{marginLeft: 'auto',marginTop:'1.6rem'}}>TEST</p>
+      <HeadingMenu styleu={{marginLeft: 'auto',marginTop:'1.6rem'}}>TEST</HeadingMenu>
     </Row>
     </>
   );
---
+++ wildoasis-app-project/src/ui/HeadingMenu.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const HeadingMenu = styled.span`
+  display:flex;
+  flex-direction:row;
+  gap:1rem;
+  margin-left: auto;
+  margin-top:'1.6rem';
+`;
+
+export default HeadingMenu;
