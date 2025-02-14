3f52147d84b76d3fc034b92330a482be6fe65fdc
JUDE-893 <ichbinarif01@gmail.com>
2025-02-14 12:51:50 +0100

    update: --feat - update a users password,name and avatar

--- wildoasis-app-project/src/services/apiAuthentication.js
+++ wildoasis-app-project/src/services/apiAuthentication.js
@@ -34,7 +34,7 @@ export async function getCurrentUser() {
 }
 
 //function that logs a user in
-export async function apiLogout({email,password}) {
+export async function apiLogout() {
 
   let { error } = await supabase.auth.signOut()
 
@@ -67,4 +67,93 @@ export async function apiSignUp({email,password,full_name}) {
   }
 
   return data;
+}
+
+// function that allow to update a user data
+export async function apiUpdateUser({password, avatar, full_name,userAvatar}) {
+
+  //1 get the data obj
+  let payload;
+  if (password) payload = {password}
+  else if (full_name) payload = {data: {full_name:full_name}};
+
+  //2 upload the data
+  const {data,error} = await supabase.auth.updateUser(payload);
+  console.log('data0', data);
+  if (error) {
+    throw new Error(error.message);
+    return null;
+  }
+  if(password) return data;
+
+  //3 determin the image url
+  if (!Boolean(userAvatar)) {
+     let imageName = avatar ? `avatar-${data.user.id}${Math.random()}` : null;
+     let imageUrl = imageName ? 'https://axudhzlpgiteizotiimm.supabase.co/storage/v1/object/public/avatars/'+imageName : null;
+     payload= {imageName:imageName,imageUrl:imageUrl}
+   }else {
+     console.log(typeof avatar,avatar);
+     let imageUrl = avatar;
+     let ar = userAvatar.split('/');
+     let imageName = ar[ar.length-1]
+     payload= {imageName:imageName,imageUrl:imageUrl}
+   }
+
+   //4 upload image
+    if (avatar && payload.imageName) {
+      console.log(Boolean(userAvatar));
+      const {error: storageError} = await (Boolean(userAvatar) ? updateStoredAvatar(payload.imageName, avatar) : uploadAvatar(payload.imageName, avatar));
+
+      if (storageError) {
+        throw new Error(storageError.message);
+        return null;
+      }
+      //5 upload the avatar url
+      if (!Boolean(userAvatar)) {
+        let imageName = payload.imageName;
+        payload = {data: {avatar: payload.imageUrl}};
+        const {data:finalData,error:updateAvatarError} = await supabase.auth.updateUser(payload);
+
+        if (updateAvatarError) {
+          await deleteStoredImage(imageName);
+          throw new Error(updateAvatarError.message);
+          return null;
+        }
+        return finalData;
+    } //end5
+  }//end4
+
+    console.log('not uploaded image', data);
+    return data
+}
+
+
+// update a stored image
+async function updateStoredAvatar(imageName, image) {
+  const { data, error} = await supabase
+    .storage
+    .from('avatars')
+    .update(imageName, image);
+      console.log('update img..');
+  if (error) throw new Error("can't replace avatar image");
+  return {error}
+}
+
+async function uploadAvatar(imageName, image) {
+  const { data, error } = await supabase
+    .storage
+    .from('avatars')
+    .upload(imageName, image);
+    console.log('upload new img..');
+
+  if (error) throw new Error("can't upload avatar image");
+  return {error};
+}
+
+// DELETE  STORED FILE
+async function deleteStoredImage(imageName) {
+  const { data, error } = await supabase
+  .storage
+  .from('cabin-images')
+  .remove([imageName])
 }
